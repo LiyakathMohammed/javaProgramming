43be42fa243d6ec1ca64fec6dcf1769f07301e0b
Liyakath <37012011+LiyakathMohammed@users.noreply.github.com>
2023-07-20 06:52:20 +0530

    Create DataTypes.java

---
+++ DataTypes.java
@@ -0,0 +1,17 @@
+package javaIntro;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		int num1; //Declaration of Variable
+//		num1 = 2345213;  //Variable assignment
+		int num1 = 2345213; //Variable Declaration and assignment
+		System.out.println(num1);
+		boolean isMajor;
+		isMajor = true;
+		System.out.println(isMajor);
+		float weight = 34.81f;
+		System.out.println(weight);
+	}
+}

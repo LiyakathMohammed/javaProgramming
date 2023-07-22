f2bce236117fffc8a6f73f9135979476ce7d5965
Liyakath <37012011+LiyakathMohammed@users.noreply.github.com>
2023-07-22 06:54:00 +0530

    Add files via upload

---
+++ ArithmeticOperators.java
@@ -0,0 +1,11 @@
+package javaIntro;
+
+public class ArithmeticOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 5, b = 17;
+		System.out.println(a - b);
+	}
+
+}
---
+++ UnaryOperators.java
@@ -0,0 +1,15 @@
+package javaIntro;
+
+public class UnaryOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		int a = -5;
+//		a = +a;
+//		a++;
+//		a--;
+		boolean a = false;
+		System.out.println(!a);
+	}
+
+}

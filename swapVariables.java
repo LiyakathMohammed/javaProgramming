22755772b79a9c5f359ffb35cbe69683532704ea
Liyakath <37012011+LiyakathMohammed@users.noreply.github.com>
2023-07-26 19:24:36 +0530

    Update and rename ConvertingValues.java to swapVariables.java

--- swapVariables.java
+++ swapVariables.java
@@ -4,18 +4,21 @@ public class ConvertingValues {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        Scanner sc = new Scanner(System.in);
+        	Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the first number:");
-		int a;
-		a = sc.nextInt();
+		int number1, number2, temp;
+		number1 = sc.nextInt();
 		System.out.println("Enter the second number:");
-		int b;
-		b = sc.nextInt();
-		int c = b;
-		b = a;
-		a = c;
+		number2 = sc.nextInt();
+		temp = number2;
+		number2 = number1;
+		number1 = temp;
 		System.out.println("The first number's value is " + a);
 		System.out.println("The second number's value is " + b);
 	}
 
 }
+
+
+// class name - SwapVariables - upper camel case
+// variable/file name - swapVariables - camel case

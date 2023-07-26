9e2b29f05bcd0493940cdcc0fe8810e3da049a4e
Liyakath <37012011+LiyakathMohammed@users.noreply.github.com>
2023-07-26 19:27:51 +0530

    Update EvenOrOdd.java

--- EvenOrOdd.java
+++ EvenOrOdd.java
@@ -5,18 +5,18 @@ public class EvenOrOdd {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the number:");
+		System.out.println("Enter the number: ");
 		int a;
 		a = sc.nextInt();
 		
 		if(a%2 == 1)
 		{
-			System.out.println(a + " " + "is an odd number");	
+			System.out.println(a + " is an odd number");	
 		}
 		
 		else if(a%2 == 0)
 		{
-			System.out.println(a + " " + "is an even number");	
+			System.out.println(a + " is an even number");	
 		}
 		else{
 			System.out.println("The number isn't even or odd");

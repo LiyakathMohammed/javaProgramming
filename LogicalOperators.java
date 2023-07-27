09fbfafce8def6c7e94928614af7f03ea9f25591
Liyakath <37012011+LiyakathMohammed@users.noreply.github.com>
2023-07-27 06:48:45 +0530

    Add files via upload

---
+++ LogicalOperators.java
@@ -0,0 +1,13 @@
+package javaIntro;
+
+public class LogicalOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10, b = 5;
+//		boolean c = (a > 5) && (b >= 5);
+		boolean c = (a > 10) || (b > 5);
+		System.out.println(c);
+	}
+
+}
---
+++ RelationalOperators.java
@@ -0,0 +1,12 @@
+package javaIntro;
+
+public class RelationalOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10, b = 6;
+		boolean c = a > b;
+		System.out.println(c);
+	}
+
+}
